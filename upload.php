5aacacfb2525144a32d6c182374381fdfeb908a5
Joabe Kachorroski <kernelkill@outlook.com>
2019-10-06 18:01:03 -0400

    Finalizando testes.

--- upload.php
+++ upload.php
@@ -20,11 +20,15 @@ $tamanho_maximo = 200000;
 //$extensao = $_FILES['arquivo']['type'];
 $extensoes = array(".png",".jpg");
 
+//Aqui passamos o nome do arquivo temporario
+$arquivo_temporario = $_FILES['arquivo']['tmp_name'];
+
 //Aqui pegamos o tamanho do arquivo que foi enviado
 $tamanho_arquivo = $_FILES['arquivo']['size'];
 
 //Passamos aqui o caminho absoluto do destino das imagens que estao sendo subidas para o servidor.
 $caminho_absoluto = "/var/www/html/upload/destino";
+
 
 if (!empty($nome_arquivo)){
 
@@ -37,21 +41,25 @@ if (!empty($nome_arquivo)){
     if (file_exists($caminho_absoluto."/".$nome_arquivo))
         die("Arquivo ja Existe");
 
-    echo "Extensão Valida, informações do Arquivo: ";
-    echo "<br>";
-    echo 'Nome do Arquivo: ' . $nome_arquivo;
-    echo "<br>";
-    echo 'Extensão: ' . $extensao;
-    echo "<br>";
+    if (move_uploaded_file($arquivo_temporario, $caminho_absoluto."/".$nome_arquivo)){
 
+        echo "Arquivo enviado com sucesso, segue informações do Arquivo";
+        echo "<br>";
+        echo "Extensão é Valida - OK";
+        echo "<br>";
+        echo 'Nome do Arquivo: ' . $nome_arquivo;
+        echo "<br>";
+        echo 'Extensão: ' . $extensao;
+        echo "<br>";
+        echo 'Tamanho: ' . $tamanho_arquivo;
+    }else{
+
+        echo "Erro ao enviar arquivo";
+    }
 
 }else{
 
     die ("Não existe um arquivo para subir!!!");
 
 }
-
-
-
-
 ?>

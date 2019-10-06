514eda165c243466fc4bc3602c5a6fea4b7e1ec4
Joabe Kachorroski <kernelkill@outlook.com>
2019-10-06 16:58:09 -0400

    Realizado a validação de tipo de extensão, tamanho e se arquivo existe.

--- upload.php
+++ upload.php
@@ -11,26 +11,34 @@ var_dump($_FILES);
 
 $nome_arquivo = $_FILES['arquivo']["name"];
 $extensao = strrchr($nome_arquivo, ".");
+$tamanho_maximo = 200000;
 //$extensao = $_FILES['arquivo']['type'];
 $extensoes = array(".png",".jpg");
+$tamanho_arquivo = $_FILES['arquivo']['size'];
+$caminho_absoluto = "/var/www/html/upload/destino";
 
 if (!empty($nome_arquivo)){
-    if (in_array($extensao, $extensoes)){
-        echo "Extensão Valida, informações do Arquivo: ";
-        echo "<br>";
-        echo 'Nome do Arquivo: ' . $nome_arquivo;
-        echo "<br>";
-        echo 'Extensão: ' . $extensao;
-        echo "<br>";
 
-    }else{
+    if (!in_array($extensao, $extensoes))
+        die ("Extensão invalida");
 
-        echo "Tipo de extensão não permiditda";
-    }
+    if ($tamanho_arquivo > $tamanho_maximo)
+        die("Arquivo muito Grande!");
+
+    if (file_exists($caminho_absoluto."/".$nome_arquivo))
+        die("Arquivo ja Existe");
+
+    echo "Extensão Valida, informações do Arquivo: ";
+    echo "<br>";
+    echo 'Nome do Arquivo: ' . $nome_arquivo;
+    echo "<br>";
+    echo 'Extensão: ' . $extensao;
+    echo "<br>";
+
 
 }else{
 
-    echo "Não existe um arquivo para subir!!!";
+    die ("Não existe um arquivo para subir!!!");
 
 }
 
